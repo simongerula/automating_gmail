e10aac8cb3d2e2c40c47322ab1bc91d9c5507015
Simon Gerula <saimonsir@gmail.com>
2020-09-15 09:49:24 -0300

    +repeat +sleep

--- automating_gmail.py
+++ automating_gmail.py
@@ -1,7 +1,22 @@
 import smtplib
+import getpass
+import time
 
-conn = smtplib.SMTP('smtp.gmail.com', 587)
-conn.ehlo()
-conn.starttls()
-conn.login('fromAdress@gmail.com','password$') # (fromAdress, password)
-conn.sendmail('fromAdress@gmail.com','toAdress@gmail.com',"Subject: Test \n\n message body \n\n -signature") # (fromAdress, toAdress, message)
+mail_from = input("Gmail From :")
+password = getpass.getpass("Password (not shown) :")
+mail_to = input("Mail To :")
+subject = input("Subject :")
+rpt =  input("Repeat (N° of emails) :")
+time = input("time between emails (seconds)")
+ctn = 0
+
+
+
+while ctn < rpt :
+    conn = smtplib.SMTP('smtp.gmail.com', 587)
+    conn.ehlo()
+    conn.starttls()
+    conn.login(mail_from,password) # (fromAdress, password)
+    conn.sendmail(mail_from, mail_to,"Subject: ", subject, "/n/n", body) # (fromAdress, toAdress, message)
+    ctn += 1
+    time.sleep(time)
