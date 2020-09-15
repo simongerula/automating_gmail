3afab18897ceafd1a86d4d72532bd07bfbc90f93
Simon Gerula <saimonsir@gmail.com>
2020-09-15 18:38:30 -0300

    correccion tiempos y asunto

--- automating_gmail.py
+++ automating_gmail.py
@@ -5,18 +5,19 @@ import time
 mail_from = input("Gmail From :")
 password = getpass.getpass("Password (not shown) :")
 mail_to = input("Mail To :")
-subject = input("Subject :")
-rpt =  input("Repeat (N° of emails) :")
-time = input("time between emails (seconds)")
+subject = ('Subject: ' + input("Subject :"))
+body = input("Body :")
+rpt =  int(input("Repeat (N° of emails) :"))
+timer = int(input("time between emails (seconds)"))
 ctn = 0
 
 
 
-while ctn < rpt :
+while (ctn < rpt) :
     conn = smtplib.SMTP('smtp.gmail.com', 587)
     conn.ehlo()
     conn.starttls()
     conn.login(mail_from,password) # (fromAdress, password)
-    conn.sendmail(mail_from, mail_to,"Subject: ", subject, "/n/n", body) # (fromAdress, toAdress, message)
+    conn.sendmail(mail_from, mail_to, subject, body) # (fromAdress, toAdress, message)
     ctn += 1
-    time.sleep(time)
+    time.sleep(timer)
